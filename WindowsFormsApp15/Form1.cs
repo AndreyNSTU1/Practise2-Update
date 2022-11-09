727d54a1da18fe0e7269e44d5af9d74d7ab79015
belet <belet@BDT-PC-002>
2022-11-09 18:42:47 +0700

    Добавьте файлы проекта.

---
+++ WindowsFormsApp15/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public int total = 0;
+        public int left_click = 0;
+        public int right_click = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs myArgs = (MouseEventArgs)e;
+            total++;
+            if (myArgs.Button == MouseButtons.Left)
+                left_click++;
+            if (myArgs.Button == MouseButtons.Right)
+                right_click++;
+            Вывод.Text = ("Общее количество : " + total.ToString()+ "Количество левой кнопкой: " +left_click.ToString()+ "Количество правой кнопкой: "+ right_click.ToString());
+        }
+    }
+}
